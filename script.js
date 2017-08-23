1b6729d0a8fbc2af9d245f32260dfdf8c635f0f9
Jen Shannon <jennifer.shannon@galvanize.com>
2017-08-23 14:23:25 -0400

    Add another example

--- script.js
+++ script.js
@@ -15,3 +15,23 @@ var user2 = {
 
 console.log("User1 username: " +user1.username);
 console.log("User2 username: " +user2.username);
+
+var person = {
+  firstName: "Homer",
+  middleName: "Jay",
+  lastName: "Simpson"
+};
+
+// "Homer"
+// "Jay"
+// "Simpson"
+for (var key in person) {
+  console.log(person[key]);
+}
+
+// firstName
+// middleName
+// lastName
+for (var key in person) {
+  console.log(key);
+}
