c52548b9b43902ae32744ce0bdf34c816355d5f7
Jen Shannon <jennifer.shannon@galvanize.com>
2017-08-23 14:22:43 -0400

    Add example

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hello")
+var myArray = [1, "Jen", true, null, 0];
+
+console.log("This is myArray[2]: "+ myArray[2]);
+
+
+var user1 = {
+  username: "shannonjen",
+  password: "1234"
+}
+
+var user2 = {
+  username: "bobdole",
+  password: "2345"
+}
+
+console.log("User1 username: " +user1.username);
+console.log("User2 username: " +user2.username);
